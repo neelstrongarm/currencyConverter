bf3508549f0a8a8eab77dc48aeae29bf9ceccef3
Adithya Neelakantan <67263572+neelstrongarm@users.noreply.github.com>
2020-07-06 11:33:49 +0530

    final commit

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
 from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
-# Converting every input currency to INR, then converting from INR to the required output currency
-OTHER_TO_INR = {'USD': 75.57, 'EURO': 85.03, 'GBP': 93.33, 'AUD': 51.91, 'INR': 0.00}
-INR_TO_OTHER = {'USD': 0.013, 'EURO': 0.012, 'GBP': 0.011, 'AUD': 0.019, 'INR': 0.00}
+names = ['CAD', 'HKD', 'ISK', 'PHP', 'DKK', 'HUF', 'CZK', 'GBP', 'RON', 'SEK', 'IDR', 'INR', 'BRL', 'RUB', 'HRK', 'JPY', 'THB', 'CHF', 'EUR', 'MYR', 'BGN', 'TRY', 'CNY', 'NOK', 'NZD', 'ZAR', 'USD', 'MXN', 'SGD', 'AUD', 'ILS', 'KRW', 'PLN']
 
 
 def lookup(frm):
-    pass
+    try:
+        response = requests.get(f"https://api.exchangeratesapi.io/latest?base={frm}")
+        response.raise_for_status()
+    except requests.RequestException:
+        return None
+    
+    try:
+        ans = response.json()
+        return ans
+    except (KeyError, ValueError, TypeError):
+        return None
+
 
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        currency = INR_TO_OTHER.keys()
+        currency = names
         # Sending values for the drop down menu on index.html
         return render_template("index.html", currency = sorted(currency))
     else:
@@ -47,14 +57,10 @@ def calculate(frm, to, amt):
     if frm == to:
         return amt
 
-    # Converting 
-    if to == 'INR':
-        return "{:.2f}".format((OTHER_TO_INR[frm] * amt))
-    if frm == 'INR':
-        return "{:.2f}".format((INR_TO_OTHER[to] * amt))
-    in_inr = OTHER_TO_INR[frm] * amt 
-    to_ans = INR_TO_OTHER[to] * in_inr
-
-    # Returning the output only upto 2 decimal places
-    fin = "{:.2f}".format(to_ans)
+    jason = lookup(frm)
+    if jason == None:
+        return render_template("apology.html", inp = "Oops, Something Went Wrong")
+    con_rate = jason['rates'][to]
+    ans = con_rate * amt
+    fin = "{:.2f}".format(ans)
     return fin
